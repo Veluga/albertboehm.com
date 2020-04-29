f7abd7eb31c5b1b36448ecb2bc649cfb4e404cfc
Veluga <albertboehm@hotmail.de>
2020-04-29 15:23:42 +0200

    Add real projects. Improve design.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,30 +2,37 @@ let listNode = document.getElementById("project-list")
 
 projects.forEach(proj => {
     let outer = document.createElement("div")
-    outer.setAttribute("class", "mainlistview")
+    outer.setAttribute("class", "project-item")
 
     let imgContainer = document.createElement("div")
     let img = document.createElement("img")
-    imgContainer.setAttribute("class", "left-div")
+    imgContainer.setAttribute("class", "img-div")
     img.src = proj["thumbnail"]
     imgContainer.appendChild(img)
 
-    let descriptionContainer = document.createElement("div")
+    let textContainer = document.createElement("div")
     let title = document.createElement("h3")
     let link = document.createElement("a")
     let description = document.createElement("p")
     let languages = document.createElement("p")
-    descriptionContainer.className = "right-div-content"
+    let tools = document.createElement("p")
+    textContainer.className = "text-div"
     description.innerHTML = proj["description"]
     link.href = proj["src"]
     link.innerHTML = proj["title"]
-    languages.innerHTML = "Languages used: " + proj["lang"]
+    languages.innerHTML = "<b>Languages</b>: " + proj["languages"]
+    tools.innerHTML = "<b>Tools</b>: " + proj["tools"]
     title.appendChild(link)
-    descriptionContainer.appendChild(title)
-    descriptionContainer.appendChild(description)
-    descriptionContainer.appendChild(languages)
+    textContainer.appendChild(title)
+    textContainer.appendChild(description)
+    if (proj["languages"]) {
+        textContainer.appendChild(languages)
+    }
+    if (proj["tools"]) {
+        textContainer.appendChild(tools)
+    }
 
     outer.appendChild(imgContainer)
-    outer.appendChild(descriptionContainer)
+    outer.appendChild(textContainer)
     listNode.appendChild(outer)
 })
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,13 +1,35 @@
+/* Demo
+
 projects = [{
     "title": "Lego Tower",
-    "lang": "Danish",
-    "description": "I made this.",
+    "languages": "Danish",
+    "tools": "Lego Builder Board",
+    "description": "I made this. I am very proud of it. You can't take it away from me. And if you will I will be sad.",
     "src": "www.lego.com",
     "thumbnail": "assets/img/project_thumbnails/lego.jpg"
 }, {
     "title": "Chocolate Chip Cookies",
-    "lang": "Flour",
+    "languages": "Flour",
+    "tools": "Oven",
     "description": "Don't question my dietary choices!",
     "src": "www.mamasbakery.com",
     "thumbnail": "assets/img/project_thumbnails/cookie.png"
+}]
+*/
+
+projects = [{
+    "title": "Ray Tracer",
+    "languages": "C++",
+    "tools": "",
+    "description": "A ray tracer that generates random scenes of spheres. \
+    Hittables and materials can be defined independently. Utilizes defocus blur and anti-aliasing.",
+    "src": "https://github.com/Veluga/ray-tracer/",
+    "thumbnail": "assets/img/project_thumbnails/ray_tracer.png"
+}, {
+    "title": "CHIP-8 Emulator",
+    "languages": "C++",
+    "tools": "",
+    "description": "Implementation of the CHIP-8 VM. Plays Pong, Invaders, Tetris, and many more.",
+    "src": "https://github.com/Veluga/chip8-emu",
+    "thumbnail": "assets/img/project_thumbnails/chip8_emu.png"
 }]
